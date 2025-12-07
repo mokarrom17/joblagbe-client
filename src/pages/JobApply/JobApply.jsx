3e81e258591830e21afe90a3b1c8bbf31d0cd93e
mokarrom17 <mokarrombasher@gmail.com>
2025-12-07 20:36:02 +0600

    axios install, jobapply page create

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router";
+import useAuth from "../../Hooks/useAuth";
 
 const JobApply = () => {
-    return (
-        <div>
-            <h3 className='text-4xl'>This is Job Apply Page</h3>
-        </div>
-    );
+  const { id: jobId } = useParams();
+  const { user } = useAuth();
+
+  console.log(jobId, user);
+
+  const handleApplyFormSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.name.value;
+    const contact = form.contact.value;
+    const description = form.description.value;
+    const resume = form.resume.value;
+
+    console.log(name, email, contact, description, resume);
+
+
+    const appliedJob = {
+        jobId,
+        applicant: user.email,
+        resume
+
+    }
+    
+  }
+  return (
+    <div>
+      <h3 className="text-4xl mb-12">This is Job Apply Page: </h3>
+      <form onSubmit={handleApplyFormSubmit} className="flex justify-center items-center">
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-2xl border p-4">
+        
+          <label className="label">Name</label>
+          <input type="text" name="name" className="input" placeholder="Full Name" />
+
+          <label className="label">Email</label>
+          <input type="email" name="email" className="input" placeholder="Personal Email" />
+
+          <label className="label">Contact Number</label>
+          <input type="phone" name="contact" className="input" placeholder="Contact Number" />
+
+          <label className="label">Description</label>
+          <input type="text" name="description" className="input" placeholder="Your Description" />
+
+          <label className="label">Resume</label>
+          <input type="url" name="resume" className="input" placeholder="Resume Link" />
+
+          <input type="submit" className="btn" value="Apply" />
+
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default JobApply;
